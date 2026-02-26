40198d79e8962d95cfa7d7d947d6068ddb5146cd
bmo-crayola <87660179+bmo-crayola@users.noreply.github.com>
2026-02-25 23:41:54 -0800

    Fixed a timer error

--- Assets/Platformer/Scripts/TimeController.cs
+++ Assets/Platformer/Scripts/TimeController.cs
@@ -4,7 +4,9 @@ using TMPro;
 public class TimeController : MonoBehaviour
 {
     public TextMeshProUGUI timeText;
-    float timeLeft = 100;
+    float timeLeft = 5;
+    bool timeUp = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +16,13 @@ public class TimeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (timeUp) return;
         timeLeft -= Time.deltaTime * 1.5f;
         timeText.text = $"TIME\n{((int)timeLeft).ToString()}";
         if (timeLeft <= 0) {
             timeText.text = "TIME\n0";
             Debug.Log("The Player ran out of time!");
+            timeUp = true;
         }
     }
 }
